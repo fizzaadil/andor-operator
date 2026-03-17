2483bbade6b90bc125143c47cb7ca9bfe38a83e4
fizzaadil <fizzaadil3455@gmail.com>
2026-03-17 18:09:02 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("all the numbers have boolean values as true")
+else:
+    print("Atleast one number has boolean vales as false")
+
+
+    a = 10
+    b = -10
+    c = 0
+    if a > 0 or b> 0:
+        print("Either of the number is greater than 0")
+    else:
+        print("no number is greator than 0")
+
+        if b >0 or c > 0:
+            print("Either of the number is greater than 0")
+        else:
+            print("no number is greater than 0")
+        
